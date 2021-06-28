9f8d27e3365fec0f4c20ca96042c21b8b1189648
Andres Noboa <andresnoboa17@hotmail.com>
2021-06-27 22:23:00 -0500

    Agregando expresiones a las reglas del parser

--- parser.py
+++ parser.py
@@ -2,14 +2,16 @@ import ply.yacc as yacc
 from analizadorLex import AnalizadorLexico
 tokens = AnalizadorLexico.tokens
 
+def p_bloque(p):
+    '''bloque : sentencia NEWLINE bloque
+              | sentencia'''
 
 def p_sentencia(p):
     '''sentencia : estructura_control
                  | asignar'''
 
 def p_asignar(p):
-    '''asignar : SYMBOL IGUAL NUMERO
-               | SYMBOL IGUAL STRING'''
+    '''asignar : SYMBOL IGUAL expresion '''
 
 def p_estructura_control(p):
     '''estructura_control : if
@@ -18,23 +20,29 @@ def p_estructura_control(p):
                            | while'''
 
 def p_if(p):
-    'if : IF PARENIZ logica PARENDER'
+    'if : IF logica THEN expresion'
 
 def p_elseif(p):
-    'elseif : ELSEIF PARENIZ logica PARENDER'
+    'elseif : ELSEIF logica THEN expresion'
 
 def p_else(p):
     'else : ELSE'
 
 def p_while(p):
-    'while : WHILE PARENIZ logica PARENDER'
+    'while : WHILE logica NEWLINE bloque'
 
 def p_logica(p):
     '''logica : SYMBOL MAYOR SYMBOL
                     | SYMBOL MENOR SYMBOL
                     | SYMBOL MAYORIGUAL SYMBOL
                     | SYMBOL MENORIGUAL SYMBOL'''
-
+def p_expresion(p):
+    '''expresion : expresion SUMAR expresion
+                 | expresion RESTAR expresion
+                 | expresion MULTIPLICAR expresion
+                 | expresion DIVIDIR expresion
+                 | NUMERO
+                 | STRING'''
 def p_error(p):
     print("Ups! tuviste un error.")
 
