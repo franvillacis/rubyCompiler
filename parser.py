db76bfadcbd3142f476729941314a94e8c21a9fb
Andres Noboa <andresnoboa17@hotmail.com>
2021-07-03 14:34:23 -0500

    Agregado funcionalidad para operaciones matematicas

--- parser.py
+++ parser.py
@@ -2,15 +2,19 @@ import ply.yacc as yacc
 from analizadorLex import AnalizadorLexico
 tokens = AnalizadorLexico.tokens
 
+#Andres Noboa
 def p_bloque(p):
     '''bloque : sentencia NEWLINE bloque
               | sentencia'''
-
+    p[0] = p[1]
+    
+#Andres Noboa
 def p_sentencia(p):
     '''sentencia : estructura_control
                  | expresion'''
+    p[0] = p[1]
 
-
+#Andres Noboa
 def p_estructura_control(p):
     '''estructura_control : if
                           | else
@@ -32,39 +36,55 @@ def p_while(p):
     '''while : WHILE PARENIZ logica PARENDER THEN expresion
              | WHILE logica NEWLINE bloque'''
 
-
+#Andres Noboa
 def p_logica_mayor(p):
     'logica : SYMBOL MAYOR SYMBOL'
 
+#Andres Noboa
 def p_logica_menor(p):
     'logica : SYMBOL MENOR SYMBOL'
 
+#Andres Noboa
 def p_logica_mayor_igual(p):
     'logica : SYMBOL MAYORIGUAL SYMBOL'
 
+#Andres Noboa
 def p_logica_menor_igual(p):
     'logica : SYMBOL MENORIGUAL SYMBOL'
 
+#Andres Noboa
 def p_expresion_sumar(p):
     'expresion : expresion SUMAR expresion'
+    p[0] = p[1] + p[3]
 
+#Andres Noboa
 def p_expresion_restar(p):
     'expresion : expresion RESTAR expresion'
+    p[0] = p[1] - p[3]
 
+#Andres Noboa
 def p_expresion_multiplicar(p):
     'expresion : expresion MULTIPLICAR expresion'
+    p[0] = p[1] * p[3]
 
+#Andres Noboa
 def p_expresion_dividir(p):
     'expresion : expresion DIVIDIR expresion'
+    p[0] = p[1] / p[3]
 
+#Andres Noboa
 def p_expresion_factor(p):
     'expresion : factor'
+    p[0] = p[1]
 
+#Andres Noboa
 def p_factor(p):
     '''factor : INT
               | FLOAT
               | STRING '''
+    p[0] = p[1]
 
+#Andres Noboa
 def p_error(p):
     print("Ups! tuviste un error.")
 
