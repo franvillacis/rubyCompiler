8563180ca285f21213db25d19bd9e6cd34e210a4
Andres Noboa <andresnoboa17@hotmail.com>
2021-07-03 19:25:15 -0500

    Agregado archivo inicial de interfaz visual

---
+++ interfaz.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+import sys
+
+app = QApplication(sys.argv)
+window = QWidget()
+window.setWindowTitle("Proyecto Lenguajes")
+window.
+window.show()
+
+sys.exit(app.exec_())
